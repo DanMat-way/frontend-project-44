11848c836e02db664be7e5e6f817fd8e72fc36cf
DanMat-way <browndanmat@gmail.com>
2026-04-21 21:44:53 +0400

    add bin

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runBrainGcd from "../src/brain-gcd.js";
+
+runBrainGcd();
---
+++ bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runBrainProgression from "../src/brain-progression.js";
+
+runBrainProgression();
