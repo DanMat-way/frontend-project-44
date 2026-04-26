6923f43444374e0efad4d2c576c8afc030cf5966
DanMat-way <browndanmat@gmail.com>
2026-04-26 14:18:04 +0400

    add

---
+++ src/games/brain-calc.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import readlineSync from "readline-sync";
+import { greetUser, askName, getRandomNumber } from '../utils.js';
+ 
+
+export default function runBrainCalc() {
+  greetUser();
+  const name = askName();
+  console.log(`Hello, ${name}!`);
+
+  const rounds = 3;
+
+  for (let i = 0; i < rounds; i++) {
+    const randomNumber1 = getRandomNumber(1, 100);
+    const randomNumber2 = getRandomNumber(1, 100);
+    const operators = ['+', '-', '*'];
+    const operator = operators[getRandomNumber(0, operators.length - 1)];
+
+    let correctAnswer;
+    switch (operator) {
+      case '+':
+        correctAnswer = randomNumber1 + randomNumber2;
+        break;
+      case '-':
+        correctAnswer = randomNumber1 - randomNumber2;
+        break;
+      case '*':
+        correctAnswer = randomNumber1 * randomNumber2;
+        break;
+    }
+
+    const answer = readlineSync.question(`Question: ${randomNumber1} ${operator} ${randomNumber2}\nYour answer: `);
+    const answerNumber = Number(answer);
+    const roundedAnswer = Math.round(answerNumber);
+
+    if (roundedAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`Wrong answer. The correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+}
+
+
+
+
+
+
---
+++ src/games/brain-even.js
@@ -0,0 +1,29 @@
+import readlineSync from "readline-sync";
+import { greetUser, askName, getRandomNumber } from '../utils.js';
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+export default function runBrainEven() {
+  greetUser();
+  const name = askName();
+  console.log(`Hello, ${name}!`);
+
+  const rounds = 3;
+  for (let i = 0; i < rounds; i++) {
+    const randomNumber = getRandomNumber(1, 100);
+    const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer (yes/no): `);
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+    if (answer.trim().toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer.trim()}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+}
