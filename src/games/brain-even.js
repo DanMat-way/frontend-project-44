58dcb7589f214846c2d6daea31c31d16a21ff357
DanMat-way <browndanmat@gmail.com>
2026-05-01 20:00:04 +0400

    changes

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,9 +1,29 @@
-const getQuestionAndAnswer = () => {
-  const number = Math.floor(Math.random() * 100);
-  const question = String(number);
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+import readlineSync from "readline-sync";
+import { greetUser, askName, getRandomNumber } from '../utils.js';
 
-  return [question, correctAnswer];
-};
+function isEven(num) {
+  return num % 2 === 0;
+}
 
-export default getQuestionAndAnswer;
+export default function runBrainEven() {
+  greetUser();
+  const name = askName();
+  console.log(`Hello, ${name}!`);
+
+  const rounds = 3;
+  for (let i = 0; i < rounds; i++) {
+    const randomNumber = getRandomNumber(1, 100);
+    const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer (yes/no): `);
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+    if (answer.trim().toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer.trim()}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+}
